da9dc6362481cfa0f4f994e0f42a95616981151d
saranibohacek <saranibohacek@seattleacademy.org>
2025-04-22 19:05:12 +0000

    submit wordle

---
+++ Projects/projectwordle.py
@@ -0,0 +1,60 @@
+import random
+
+# Pick a word at random
+word_list = ["rings","heart","brandy","nails","pearl", "stars","owala","funny","nicer","sweet","beach"]
+hidden_word = random.choice(word_list)
+
+# Repeat for 6 guesses
+for i in range(6):
+    # Guess a word
+    guess_word = input()
+    output = ""
+
+    # First letter (in python, counting starts at 0 not 1)
+    if guess_word[0] == hidden_word[0]:
+        output += "🟩"
+    elif guess_word[0] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+    #second letter
+    if guess_word[1] == hidden_word[1]:
+        output += "🟩"
+    elif guess_word[0] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+    #third letter
+    if guess_word[2] == hidden_word[2]:
+        output += "🟩"
+    elif guess_word[0] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+    #fourth letter
+    if guess_word[0] == hidden_word[0]:
+        output += "🟩"
+    elif guess_word[0] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+
+    #fifth letter
+    if guess_word[0] == hidden_word[0]:
+        output += "🟩"
+    elif guess_word[0] in hidden_word:
+        output += "🟨"
+    else:
+        output += "⬛"
+    
+
+    # Result
+    print(output)
+    if output == "🟩🟩🟩🟩🟩":
+        print("You win")
+        break
+
+print(f"You used {i+1} guesses")
